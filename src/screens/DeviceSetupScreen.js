45970381d2d7fe51976af90a0fad0d413e6f7873
Ishank <iishanksharma@gmail.com>
2026-02-08 21:18:12 +0530

    retrieval screens added

--- src/screens/DeviceSetupScreen.js
+++ src/screens/DeviceSetupScreen.js
@@ -10,7 +10,7 @@ import {
   ScrollView,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import DateTimePicker from "@react-native-community/datetimepicker";
+// import DateTimePicker from "@react-native-community/datetimepicker";
 
 import { LightTheme, DarkTheme } from "../constants/theme";
 import { setCurrentStep } from "../storage/progressStorage";
